46c36e992e39414f9ddbfe33900ada5e2c467c6d
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-05-04 15:40:30 +0200

    Needleman–Wunsch experiment

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,8 @@ import Language.ECMAScript3.Parser
 import Language.ECMAScript3.PrettyPrint
 import Language.ECMAScript3.Syntax
 
+import Match
+
 main :: IO ()
 main = do
   parsed <- parseFromFile "test.js"
@@ -15,6 +17,7 @@ idEq (Id _ i) (Id _ i') = i == i'
 
 expDep :: Expression a -> Expression a -> [Id a]
 expDep (ArrayLit _ es) x = concatMap (flip expDep x) es
+-- expDep (ObjectLit _ props) x = concatMap (flip expDep x) es
 
 isDep :: Expression a -> Statement a -> [Id a]
 isDep e (BlockStmt _ ss) = concatMap (isDep e) ss
---
+++ src/Match.hs
@@ -0,0 +1,39 @@
+module Match where
+
+import Control.Monad
+
+set1 :: Int -> a -> [a] -> [a]
+set1 i x xs = take i xs ++ [x] ++ drop (i + 1) xs
+
+set :: Int -> Int -> a -> [[a]] -> [[a]]
+set x y v vs = set1 x (set1 y v (vs !! x)) vs
+
+walk :: Eq a => [a] -> [a] -> [[Int]]
+walk a b = foldl (\a (i, j) -> loop i j a) f [ (i, j) | i <- [1..length a - 1], j <- [1..length b - 1]]
+  where
+    f = take (length a) $ repeat (take (length b) $ repeat 0)
+
+    loop i j f = set i j (max match (max insert delete)) f
+      where
+        match = f !! (i - 1) !! (j - 1) + sim (a !! i) (b !! j)
+        delete = f !! (i - 1) !! j
+        insert = f !! i !! (j - 1)
+
+sim :: Eq a => a -> a -> Int
+sim a b | a == b = 1
+        | otherwise = 0
+
+align :: (Eq a, Show a) => [a] -> [a] -> [[Int]] -> (String, String)
+align a b f = go (length a - 1) (length b - 1) ("", "")
+  where
+    go 0 0 (ala, alb) = (ala, alb)
+    go i j (ala, alb)
+      | i > 0 && j > 0 && (f !! i !! j) == (f !! (i - 1) !! (j - 1)) + sim (a !! i) (b !! j)
+        = go (i - 1) (j - 1) (show (a !! i) ++ ala, show (b !! j) ++ alb)
+      | i > 0 && (f !! i !! j) == (f !! (i - 1) !! j)
+        = go (i - 1) j (show (a !! i) ++ ala, "-" ++ alb)
+      | otherwise
+        = go i (j - 1) ("-" ++ ala, show (b !! j) ++ alb)
+
+nw :: (Eq a, Show a) => [a] -> [a] -> (String, String)
+nw a b = align a b (walk a b)
