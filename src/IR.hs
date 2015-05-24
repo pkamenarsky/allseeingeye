e2fcf3f81bfaba5a3e4d9b18339d3e3763191c6e
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-05-24 18:03:37 +0200

    Inline WIP

--- src/IR.hs
+++ src/IR.hs
@@ -1,8 +1,16 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, OverloadedStrings, TupleSections, TypeSynonymInstances #-}
+{-# LANGUAGE DeriveDataTypeable, FlexibleInstances, MultiParamTypeClasses, OverloadedStrings, TupleSections, TypeSynonymInstances #-}
 
 module IR where
 
-import Graph
+import           Control.Applicative          ((<$>), (<*>), pure)
+
+import           Data.Data
+import qualified Data.Map                     as M
+import           Data.Typeable
+
+import           Data.Generics.Uniplate.Data
+
+import           Graph
 
 type Name = String
 
@@ -29,7 +37,7 @@ data G a = G_Const String
          | G_Return (G a)
          | G_Ctrl (G a) (G a)
          | G_Nop
-         deriving (Eq, Ord, Show)
+         deriving (Eq, Ord, Data, Typeable, Show)
 
 instance Graph (G a) String where
   node (G_Const c)    = ("const " ++ c, [])
@@ -71,19 +79,39 @@ genG ctx (Ctrl x s) = (G_Ctrl ge gs, ctx')
 cmpG :: G a -> G a -> Bool
 cmpG = undefined
 
+inlineLambdas :: Data a => G a -> Maybe (G a)
+inlineLambdas (G_Call l@(G_Lambda ns g) xs)
+  | length ns /= length xs = Nothing
+  | otherwise              = Just $ transform f g
+  where
+    nxmap = M.fromList $ zip ns xs
+    f (G_Arg n') | Just x' <- M.lookup n' nxmap = G_Decl n' x'
+                 | otherwise                    = G_Arg n'
+inlineLambdas g@(G_Call f xs)  = G_Call <$> inlineLambdas f <*> sequence (map inlineLambdas xs)
+inlineLambdas g@(G_Const c)    = Just $ g
+inlineLambdas g@(G_ExtRef c)   = Just $ g
+inlineLambdas g@(G_Lambda n f) = G_Lambda <$> pure n <*> inlineLambdas f
+inlineLambdas g@(G_Decl n x)   = G_Decl <$> pure n <*> inlineLambdas x
+inlineLambdas g@(G_Arg n)      = Just $ g
+inlineLambdas g@(G_Assign n x) = G_Assign <$> pure n <*> inlineLambdas x
+inlineLambdas g@(G_Return x)   = G_Return <$> inlineLambdas x
+inlineLambdas g@(G_Ctrl i x)   = G_Ctrl <$> inlineLambdas i <*> inlineLambdas x
+inlineLambdas g@(G_Nop)        = Just $ g
+
+
+
+
+
+
+
+
+
+
+
 type RefCtx = [Name]
 
-gatherDecls :: G a -> [Name] -> [Name]
-gatherDecls (G_Const _)    ctx = ctx
-gatherDecls (G_ExtRef c)   ctx = ctx
-gatherDecls (G_Call f xs)  ctx = foldr gatherDecls (gatherDecls f ctx) xs
-gatherDecls (G_Lambda n f) ctx = ctx
-gatherDecls (G_Decl n x)   ctx = n:gatherDecls x ctx
-gatherDecls (G_Arg n)      ctx = n:ctx
-gatherDecls (G_Assign n x) ctx = gatherDecls x ctx
-gatherDecls (G_Return x)   ctx = gatherDecls x ctx
-gatherDecls (G_Ctrl i x)   ctx = gatherDecls i $ gatherDecls x ctx
-gatherDecls (G_Nop)        ctx = ctx
+gatherDecls :: Data a => G a -> [Name] -> [Name]
+gatherDecls g _ = [ n | (G_ExtRef n) <- universe g ]
 
 isPure :: RefCtx -> G a -> Bool
 isPure ctx (G_Const c)    = True
