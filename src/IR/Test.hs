324aaccb672e93ffee8883cfc5a5d409778eb638
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-06-03 16:04:31 +0200

    Lambda WIP

--- src/IR/Test.hs
+++ src/IR/Test.hs
@@ -11,19 +11,19 @@ import           Graph
 import           IR
 import           IR.Serialize
 
-pr2 = Block $ P
+pr2 = P
        [ Decl "x" (Const "5")
        , Decl "y" (Const "6")
        , Return (Call (Const "+") [(Ref "x"), (Ref "y")])
        ]
 
-pr1 = Block $ P
+pr1 = P
        [ Decl "x" (Const "5")
        , Decl "y" (Const "6")
        , Return (Call (Const "+") [Ref "x", Ref "x", Ref "y", Call (Const "-") [Ref "x", Ref "y"]])
        ]
 
-pr = Block $ P
+pr = P
        [ Decl "world" (Const "world")
        , Decl "x" (Const "5")
        , Decl "y" (Const "6")
@@ -32,17 +32,17 @@ pr = Block $ P
        , Return (Call (Const "IO") [Call (Const "+") [(Ref "world"), (Ref "x"), (Ref "y")], Ref "world"])
        ]
 
-pr3_old = Block $ P
+pr3_old = P
        [ Decl "world" (Const "world")
        , Decl "x" (Const "5")
        , Decl "y" (Const "7")
-       , Decl "writeFile" (Lambda ["name", "contents", "world"] (Block $ P [Return (Call (Ref "IO") [Ref "name", Ref "contents", Ref "world"])]))
+       , Decl "writeFile" (Lambda ["name", "contents", "world"] (P [Return (Call (Ref "IO") [Ref "name", Ref "contents", Ref "world"])]))
        , Assign "world" (Call (Ref "rand") [Ref "x", Ref "world"])
        , Assign "world" (Call (Ref "writeFile") [Ref "y", Ref "x", Ref "world"])
        , Return (Call (Ref "IO") [Call (Ref "+") [Ref "x", Ref "y"], Ref "world"])
        ]
 
-pr3 = Block $ P
+pr3 = P
        [ Decl "world" (Const "world")
        , Decl "a" (Const "8")
        , Decl "b" (Const "8")
@@ -51,39 +51,9 @@ pr3 = Block $ P
        , Decl "x" (Const "6")
        , Decl "y" (Const "7")
        , Decl "z" (Const "8")
-       -- , Decl "writeFile" (Lambda ["name", "contents", "world"] (Block $ P [Return (Call (Ref "IO") [Ref "name", Ref "contents", Ref "world"])]))
-       , Decl "writeFile" (Lambda ["name", "contents"] (Block $ P [Return (Call (Ref "IO") [Ref "name", Ref "contents", Ref "world"])]))
+       -- , Decl "writeFile" (Lambda ["name", "contents", "world"] (P [Return (Call (Ref "IO") [Ref "name", Ref "contents", Ref "world"])]))
+       , Decl "writeFile" (Lambda ["name", "contents"] (P [Return (Call (Ref "IO") [Ref "name", Ref "contents", Ref "world"])]))
        , Assign "world" (Call (Ref "rand") [Ref "x", Ref "world"])
        , Assign "world" (Call (Ref "writeFile") [Ref "y", Ref "x", Ref "world"])
        , Return (Call (Ref "IO") [Call (Ref "+") [Ref "z", Ref "x", Ref "y"], Ref "c", Ref "world"])
        ]
-
-getLocal x = Call (Const $ "local[" ++ x ++ "]") [Ref "locals"]
-
-pr4 = Block $ P
-       [ Decl "locals" (Const "locals + world")
-       , Assign "locals" (Call (Const "rand") [getLocal "x", getLocal "world"])
-       , Assign "locals" (Call (Const "writeFile") [getLocal "y", getLocal "x", getLocal "world"])
-       , Return (Call (Const "IO") [Call (Const "+") [getLocal "x", getLocal "y"], getLocal "world"])
-       ]
-
-g :: G2 Label ()
-g = execState (genG2fromS ectx2 pr3) emptyG2
-
--- ge = removeSubgraph 16 g
-ge = removeOrphanDecls g
--- ge = g
-
-nodes = "var nodes = " ++ (B.unpack $ encode ge)
-
-{-
-g :: G ()
-g = fromMaybe G_Nop $ inlineLambdas $ fst $ genG ectx pr3_old
-
-nodes = "var nodes = " ++ (B.unpack $ encode $ serializeG g)
-
-ts :: [String]
-ts = topsort g
-
-pts = intercalate " -> " ts
--}
