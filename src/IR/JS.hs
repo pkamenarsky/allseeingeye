5899663e30a2b9412133bef9dcbd95bdb90750cb
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-06-05 21:23:52 +0200

    JS to IR WIP

--- src/IR/JS.hs
+++ src/IR/JS.hs
@@ -7,8 +7,9 @@ import           IR
 setA :: L -> Int -> L -> L
 setA arr i e = (App (App (App (Var "setA") arr) (Cnst $ show i)) e)
 
-convert :: Expression a -> L
-convert (StringLit a lit) = Cnst lit
+convert :: Expression a -> L -> L
+convert (StringLit a lit) cnt = Cnst lit
+{-
 convert (RegexpLit a lit glb csi) = Cnst lit
 convert (NumLit a lit) = Cnst $ show lit
 convert (IntLit a lit) = Cnst $ show lit
@@ -16,7 +17,7 @@ convert (BoolLit a lit) = Cnst $ show lit
 convert (NullLit a) = Var "null"
 convert (ArrayLit a es) = App (Lam "array" ((setA (Var "array") 0 (convert $ head es))))
                               (App (Var "newArray") (Var "world"))
-{-
+
 convert (ObjectLit a [(Prop a, Expression a)])
 convert (ThisRef a)
 convert (VarRef a (Id a))
@@ -27,8 +28,10 @@ convert (PrefixExpr a PrefixOp (Expression a))
 convert (UnaryAssignExpr a UnaryAssignOp (LValue a))
 convert (InfixExpr a InfixOp (Expression a) (Expression a))
 convert (CondExpr a (Expression a) (Expression a) (Expression a))
-convert (AssignExpr a AssignOp (LValue a) (Expression a))
-convert (ListExpr a [Expression a])
+-}
+convert (AssignExpr a op (LVar a' lv) e) cnt = let cnt' = App (Lam lv cnt) (convert e cnt') in cnt'
+convert (ListExpr a es) cnt = foldr convert cnt es
+{-
 convert (CallExpr a (Expression a) [Expression a])
 convert (FuncExpr a (Maybe (Id a)) [Id a] [Statement a])
 -}
