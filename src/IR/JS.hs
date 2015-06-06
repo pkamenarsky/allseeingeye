421ddd44ccf727e587d47d6540adf5586365f68d
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-06-06 07:03:15 +0200

    Lambda WIP

--- src/IR/JS.hs
+++ src/IR/JS.hs
@@ -17,42 +17,6 @@ import           IR
 setA :: L -> Int -> L -> L
 setA arr i e = (App (App (App (Var "setA") arr) (Cnst $ show i)) e)
 
-{-
-convert2 :: [Expression a] -> L
-convert2 [(StringLit a lit)] = Cnst lit
-{-
-convert (RegexpLit a lit glb csi) = Cnst lit
-convert (NumLit a lit) = Cnst $ show lit
-convert (IntLit a lit) = Cnst $ show lit
-convert (BoolLit a lit) = Cnst $ show lit
-convert (NullLit a) = Var "null"
-convert (ArrayLit a es) = App (Lam "array" ((setA (Var "array") 0 (convert $ head es))))
-                              (App (Var "newArray") (Var "world"))
-
-convert (ObjectLit a [(Prop a, Expression a)])
-convert (ThisRef a)
--}
-convert2 [(VarRef a (Id a' ref))] cnt = Var ref
-{-
-convert (DotRef a (Expression a) (Id a))
-convert (BracketRef a (Expression a) {- container -} (Expression a) {- key -})
-convert (NewExpr a (Expression a) {- constructor -} [Expression a])
-convert (PrefixExpr a PrefixOp (Expression a))
--}
-convert2 (UnaryAssignExpr a PrefixInc (LVar a' lv)) cnt = App (Lam lv cnt) ((Var lv))
-{-
-convert (InfixExpr a InfixOp (Expression a) (Expression a))
-convert (CondExpr a (Expression a) (Expression a) (Expression a))
--}
-convert2 (AssignExpr a op (LVar a' lv) e) cnt =
-  let cnt' = convert e cnt in App (Lam lv cnt') cnt'
-convert2 (ListExpr a es) cnt = foldr convert cnt es
-{-
-convert (CallExpr a (Expression a) [Expression a])
-convert (FuncExpr a (Maybe (Id a)) [Id a] [Statement a])
--}
--}
-
 convert :: Expression a -> L -> L
 convert (StringLit a lit) cnt = Cnst lit
 {-
