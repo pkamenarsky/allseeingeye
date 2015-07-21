3e9f227f6d5c80a04ddf38d6993b14f69fd023c3
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-07-21 16:14:49 +0200

    Back world rewrite

--- src/IR.hs
+++ src/IR.hs
@@ -23,10 +23,7 @@ import System.Random
 
 import Debug.Trace
 
-data Name = Local  { name :: String }
-          | Bound  { name :: String }
-          | Global { name :: String }
-          deriving (Eq, Ord, Data, Typeable)
+type Name = String
 
 data CtrlType
 
@@ -48,16 +45,8 @@ data P = P [S] deriving (Eq, Ord, Data, Typeable)
 data L a = Cnst a String
          | Var a Name
          | Extrn a Name
-         -- | App a (L a) [L a]
          | App a (L a) (L a)
-         -- | Lam a [Name] (L a)
          | Lam a Name (L a)
-         | Let a Name (L a) (L a)
-         | Letlocal a Name (L a) (L a)
-         | Rho a [Name] (L a)
-         -- | Merge a (M.Map String (L a))
-         | Merge a [(String, L a)]
-         | Hold a Name (L a)
          deriving (Eq, Ord, Data, Typeable)
 
 data W = W { unW :: M.Map Name (L W) } deriving (Eq, Ord, Data, Typeable)
@@ -71,22 +60,6 @@ result = "ρ"
 object = "σ"
 world = "ω"
 
-{-
--- multiarg
-sToE :: E -> L W
-sToE (Const c)     = Cnst w c
-sToE (Ref r)       = Var w r
-sToE (Call f xs)   = App w (sToE f) (map sToE xs)
-sToE (Lambda ns p) = Lam w ns (sToP p)
-
-sToP :: P -> L W
-sToP (P [])              = error "no return"
-sToP (P (Decl n e:ps))   = App w (Lam w [n] (sToP (P ps))) [sToE e]
-sToP (P (Assign n e:ps)) = App w (Lam w [n] (sToP (P ps))) [sToE e]
-sToP (P (Return e:_))    = sToE e
-sToP (P (Ctrl e p:_))    = error "ctrl"
--}
-
 sToE :: E -> L String
 sToE (Const c)     = Cnst w c
 sToE (Ref r)       = Var w r
@@ -97,180 +70,13 @@ sToP :: P -> L String
 sToP (P [])              = error "no return"
 sToP (P (Decl n e:ps))   = App w (Lam w n (sToP (P ps))) (sToE e)
 sToP (P (Assign n e:ps)) = App w (Lam w n (sToP (P ps))) (sToE e)
--- sToP (P (Decl n e:ps))   = Let w n (sToE e) (sToP (P ps))
--- sToP (P (Assign n e:ps)) = Let w n (sToE e) (sToP (P ps))
 sToP (P (Return e:_))    = sToE e
 sToP (P (Ctrl e p:_))    = error "ctrl"
-
-unTag :: L a -> a
-unTag (Cnst w _)    = w
-unTag (Var w _)     = w
-unTag (Extrn w _)   = w
-unTag (App w _ _)   = w
-unTag (Let w _ _ _) = w
-unTag (Lam w _ _)   = w
-unTag (Merge w _)   = w
-
-tag :: a -> L a -> L a
-tag w (Cnst _ c)    = Cnst w c
-tag w (Var _ v)     = Var w v
-tag w (Extrn _ v)   = Extrn w v
-tag w (App _ f x)   = App w f x
-tag w (Let _ k v e) = Let w k v e
-tag w (Lam _ n f)   = Lam w n f
-tag w (Merge _ ms)  = Merge w ms
 
 fixpoint :: Eq a => (a -> a) -> a -> a
 fixpoint f a | a == a' = a
              | otherwise = fixpoint f a'
   where a' = f a
-
-{-
--- multiarg
-subst :: Show a => Name -> L a -> L a -> L a
--- subst n e _ | trace ("s: " ++ show n ++ ", e: " ++ show e) False = undefined
-subst _ _ e'@(Cnst w _) = e'
-{-
-subst n e e'@(Hold w n' _) | name n == name n'   = Hold w n' e
-                           | otherwise           = e'
--}
-subst n e e'@(Var w n') | name n == name n'   = e
-                        | otherwise           = e'
-subst n e e'@(App w f x)  = App w (subst n e f) (map (subst n e) x)
-subst n e e'@(Lam w ns f) | name n `elem` map name ns = Lam w ns f -- don't substitute a bound var
-                          | otherwise                 = Lam w ns (subst n e f)
-subst n e e'@(Merge w xs) = Merge w (map (second $ subst n e) xs)
--}
-
-{-
-ρ = f …
-
-(λρ → …) (⤚ x y z) ≈ (λx → (λy → (λz → …) z) y) x
-
-add5(array) {
-  array.push_back(5);
-  log();
-  g = f(array);
-  return 666;
-}
-
-a = add5(x);
-
-    v
-
-add5(array, ω) {
-  array = array.push_back(5);
-  ω = log(ω);
-  g = f(array);
-  return ⤚(666, ω, array, g);
-}
-
-ρ<a, ω> = add5(x, ω); // expands to a, ω, x, g = add5(x, ω);
-
---
-
-(λρ<a, ω> → …) f
-
-if f unbound | (λρ → (λa → (λω → …) ↖2(ρ)) ↖1(ρ)) f
-otherwise    | (λρ<a, ω> →  …) (⤚ x y U V)
-             ≈ (λa → (λω → (λU → (λV → …) V) U) y) x
-
----
-
-lambda ≈ let x = a in letlocal y = b in … in ⤚(x, …) | x not local
-
-let x = ⤚(Ta, Tb, Tω) in ... ≈ let a = Ta in let b = Tb in let ω = Tω in …
-
----
-
-a = f(x)      | ρ<a, ω> = f(x, ↖ω), use ↖a instead of a
-n.push(x)     | ρ<n, ω> = push(n, x, ↖ω)
-r = c.push(x) | ρ<r, c, ω> = push(c, x, ↖ω)
-
-let[C] x = a in … ≈ ((λx → …) a)[x:C]
-(f a)[x, y, z, …] ≈ ⤚(f a) x y z …
-
----
-
-(λX → …) a ≈ … [X → a<X>]
-
-   (λX → (λY → + X Y) b) a
- ≈ (λX → ⤚(+ X b<Y>) b<Y>) a
- ≈ ⤚a<X> (⤚(+ X b<Y>) b<Y>)
- ≈ ⤚(+ X b<Y>) b<Y> a<X>
-
-   (λX → (λY → 5) b) a
- ≈ (λX → ⤚5 b<Y>) a
- ≈ ⤚5 b<Y> a<X>
-
----
-
-(λx → ⤚ (λu → …) y)
-(λx → ⤚ (λu → ⤚ u v) y)
-(λx → ⤚ x y) (⤚ u v)
-
-=== ⤚ RULES ===
-(⤚ x y) u                 ≈ ⤚ (x u) y
-u (⤚ x y)                 ≈ ⤚ (u x) y
-⤚ (⤚ x y) (⤚ u v)         ≈ ⤚ x y u v
-===============
-
-(⤚ x y) (⤚ u v)           ≈ ⤚ (x (⤚ u v)) y ≈ ⤚ (⤚ (x u) v) y ≈ ⤚ (x u) v y
-
--}
-
-{-
-replaceret :: ([L a] -> L a -> L a) -> L a -> L a
-replaceret = go []
-  where
-    go ctx f (App w e@(Lam w2 ns lam) xs)
-      | length ns == length xs = App w (Lam w2 ns (go (e:ctx) f lam)) (map (go [] f) xs)
-      | otherwise              = error "replaceret: curried application"
-    go ctx f e@(Lam w ns lam)  = f ctx (Lam w ns (go [] f lam))
-    go ctx f e@(App w app xs)  = f ctx (App w (go [] f app) (map (go [] f) xs))
-    go ctx f e                 = f ctx e
-
-boundmerge :: Show a => L a -> L a
-boundmerge = replaceret f
-  where
-    f ctx e
-      -- | trace ("\nm: " ++ show e) False = undefined
-      | null bounds = e
-      | otherwise   = Merge (unTag e) $ ("ρ", e):map (\v -> (name v, Var (unTag e) v)) bounds
-      where bounds = concatMap (\(Lam _ ns _) -> filter isBound ns) ctx
-            isBound (Global _) = True
-            isBound (Bound  _) = True
-            isBound (Local  _) = False
--}
-
-boundmerge :: Show a => L a -> L a
-boundmerge (Cnst w c) = Cnst w c
-boundmerge (Var w n) = Var w n
-boundmerge (Let w (Bound k) v e)
-  = Let w (Local k) (boundmerge v) (Merge w [("ρ", boundmerge e), (k, Var w (Local k))])
-boundmerge (Let w k v e) = Let w k (boundmerge v) (boundmerge e)
-boundmerge (Lam w (Bound n) x)
-  = Lam w (Local n) (Merge w [("ρ", boundmerge x), (n, Var w (Local n))])
-boundmerge (Lam w n x) = Lam w n (boundmerge x)
-boundmerge (App w f x) = App w (boundmerge f) (boundmerge x)
-
-{-
-  (λf → ⤚ ρ<f 5> c d) (λx → ⤚ ρ<x> a b)
-≈ ⤚ ρ<⤚ ρ<x> a b> c d
-≈ ⤚ ρ<x> a b c d
-
-but this is automatically done by the destruct rule:
-
-  (λρ<a, b> → …) (⤚ x y z)
-≈ (λa → (λb → (λz → …) z) y) x
-
--}
-
-zip' :: [a] -> [b] -> [(Maybe a, Maybe b)]
-zip' [] []         = []
-zip' (x:xs) (y:ys) = (Just x, Just y) :zip' xs ys
-zip' (x:xs) []     = (Just x, Nothing):zip' xs []
-zip' [] (y:ys)     = (Nothing, Just y):zip' [] ys
 
 trace_tag str x = trace (str ++ show x) x
 
@@ -295,103 +101,31 @@ subst_dbg n e e' = trace_n ("☀☀ [" ++ show n ++ "=" ++ show e ++ "]") e' $ s
 
 -- subst :: Name -> L a -> L a -> L a
 subst :: Name -> L String -> L String -> L String
--- FIXME: ρ
-subst n e e' | name n == unTag e' = if name n == "ρ" then e else tag (name n) e
 subst _ _ e'@(Cnst _ _) = e'
-subst n e e'@(Hold w n' h) | name n == name n' = Hold w n' e
-                           | otherwise         = Hold w n' (subst n e h)
--- subst n e e'@(Var w n') | name n == name n' = Hold w n' e
--- FIXME: ρ
-subst n e e'@(Var w n') | name n == name n' = if name n == "ρ" then e else tag (name n) e
-                        | otherwise         = e'
+subst n e e'@(Var w n') | n == n'   = e
+                        | otherwise = e'
 subst n e e'@(App w f x) = App w (subst n e f) (subst n e x)
-subst n e e'@(Let w n' k v) | name n == name n' = Let w n' (subst n e k) v
-                            | otherwise         = Let w n' (subst n e k) (subst n e v)
-subst n e e'@(Lam w n' f) | name n == name n'  = Lam w n' f
-                          | otherwise          = Lam w n' (subst n e f)
--- subst n e e'@(Merge w (("ρ", r):ms))  = Merge w (("ρ", subst n e r):ms)
--- subst n e e'@(Merge w ms)  = Merge w (map f ms)
-subst n e e'@(Merge w (("ρ", r):ms))  = Merge w (("ρ", subst n e r): map f ms)
-  -- don't subst twice
-  where f (k, Var w x) = (k, subst n e (Var w x))
-  -- where f (k, e') = (k, subst n e e')
-        f e            = e
+subst n e e'@(Lam w n' f) | n == n'   = Lam w n' f
+                          | otherwise = Lam w n' (subst n e f)
 
 -- normalize :: Show a => L a -> L a
 normalize :: L String -> L String
 -- normalize e | trace (show e) False = undefined
 normalize (Cnst w c)   = Cnst w c
 normalize (Var w n)    = Var w n
-normalize (Hold w n e) = Hold w n $ normalize e
-normalize e@(Lam w (Bound n) x)
-  = normalize $ trace_n "lift merge" e $ Lam w (Local n) (Merge w [("ρ", normalize x), (n, Var w (Local n))])
-normalize e'@(Let w k v e) = go (normalize v)
-  where -- go v' | name k == "ω" = normalize $ trace_n "let ω" e' $ subst_dbg k v' e
-        go v'@(Merge w2 (("ρ", r):ms))
-          = normalize $ trace_n ("let merge{" ++ show v' ++ "}") e'
-                      $ foldl (\cnt (n, v) -> Let w (Local n) v cnt)
-                              (Let w k r e)
-                              ms
-        go v' = normalize $ trace_n "let" e' $ subst_dbg k v' e
--- normalize e'@(App w (Lam w2 n f) x) | name n == "ρ" = trace_n "rholam" e' $ App w (Lam w2 n f) (normalize x)
-normalize e'@(App w (Lam w2 n@(Local _) e) x) | isNotMerge x = normalize $ trace_n ("subst simple[" ++ show n ++ "=" ++ show x ++ "]") e' $ subst_dbg n (tag "" (normalize x)) e
-  where isNotMerge (Merge _ _) = False
-        isNotMerge _           = True
 normalize e'@(App w f x) = go (normalize f) (normalize x)
-  where go e''@(Lam w2 n f) e'''@(Merge w3 (("ρ", r):ms))
-          {-
-          = normalize $ trace_n "lam merge" e'
-                      $ foldl (\cnt (n, v) -> Let w (Local n) v cnt)
-                              (Let w n r f)
-                              ms
-          -}
-          = normalize $ trace_n ("lam merge{" ++ show e''' ++ "}") e''
-                      $ foldl (\cnt (n, v) -> App w (Lam w2 (Bound n) cnt) v)
-                              (App w (Lam w n f) r)
-                              ms
-        -- u (⤚ x y)                 ≈ ⤚ (u x) y
-        go e''@(Lam _ n e) x' = normalize $ trace_n ("subst[" ++ show n ++ "=" ++ show x' ++ "]") e'' $ subst_dbg n (tag "" x') e
-        go f (Merge w2 (("ρ", r):ms))
-          = normalize $ trace_n "app merge" e' $ Merge w2 (("ρ", App w f r):ms)
-        -- (⤚ x y) u                 ≈ ⤚ (x u) y
-        go (Merge w2 (("ρ", r):ms)) x
-          = normalize $ trace_n "merge app" e' $ Merge w2 (("ρ", App w r x):ms)
+  where go e''@(Lam _ n e) x' = normalize $ trace_n ("subst[" ++ show n ++ "=" ++ show x' ++ "]") e'' $ subst_dbg n x e
         go f' x'              = trace_n "app" e' $ App w f' x'
 normalize e@(Lam w n f) = trace_n "lam" e $ Lam w n (normalize f)
--- ??
--- normalize e'@(Merge w (("ρ", Let w2 k v e):xs)) = normalize $ trace_n "merge out" e' $ Let w2 k v (Merge w (("ρ", e):xs))
-normalize (Merge w xs) = Merge w (map (second normalize) xs)
--- normalize e'@(Let w k v e) | name k == "ρ" = normalize $ Let w (Local "ρ") (normalize v) e
-
-{-
-f { a = 6; }
-g { a = 8; }
-g(f()) -> a = 8
-
-g(); f();
-((GOpAdd La) (⤚ ρ{(((λLx → (λLω → (⤚ ρ{Lx} a{6<a>}))) 4) Lω)<x>} a{8<a>}))
-
-f (⤚… a) : ⤚f… a and delete a from all inner merges?
-(⤚… a) f : ?
-
--}
 
 simplify :: Show a => L a -> L a
 simplify = undefined
--- simplify = normalize . boundmerge
 
 u = undefined
 app = App u
 lam = Lam u
 var = Var u
-global = Global
-local = Local
-bound = Bound
-varglobal = Var u . Global
-varlocal = Var u . Local
-varbound = Var u . Bound
 cnst = Cnst u
-merge = Merge u
 
 -- rule6 = lam [local "x"] (varlocal "a") `app` [merge [("ρ", cnst "666"), ("a", cnst "777")]]
 {-
@@ -470,11 +204,6 @@ instance Show W where
 #endif
 -}
 
-instance Show Name where
-  show (Global n) = "G" ++ n
-  show (Local n) =  "L" ++ n
-  show (Bound n) =  "B" ++ n
-
 showtag tag | null tag  = ""
             | otherwise = "<" ++ tag ++ ">"
 
@@ -482,9 +211,6 @@ showtag tag | null tag  = ""
 instance Show (L String) where
   show (Cnst a c)   = c ++ showtag a
   show (Var a n)    = show n
-  show (Hold a n e) = "H{" ++ show n ++ ":" ++ show e ++ "}"
-  show (Let a k v e)= "let " ++ show k ++ " = " ++ show v ++ " in " ++ show e
-  -- show (Hold a n e) = show e
   show (Extrn a n)  = "⟨" ++ show n ++ "⟩"
 
 #if 1
@@ -502,4 +228,3 @@ instance Show (L String) where
   show (App a f x)                          = "" ++ show f ++ " " ++ show x ++ ""
   show (Lam a n f)                          = "λ" ++ show n ++ " → " ++ show f ++ ""
 #endif
-  show (Merge a xs)                         = "(⤚ " ++ intercalate " " (map (\(k, v) -> k ++ "{" ++ show v ++ "}") xs) ++ ")"
--- src/IR/JS.hs
+++ src/IR/JS.hs
@@ -19,50 +19,35 @@ import           Language.ECMAScript3.Syntax.Annotations
 
 import           IR
 
-type VarIndex = (String, [Int])
+type VarIndex = [Int]
 
 data Context = Context
   { unSS      :: [S] -> [S]
   , unWorld   :: M.Map String VarIndex
-  , unIndex   :: [Int]
+  , unIndex   :: VarIndex
   , unArgs    :: S.Set String
-  , unAssign  :: S.Set Name
-  , unLocals  :: S.Set String
-  , unF       :: M.Map String E
   }
 
-newDecl :: String -> State Context VarIndex
+newArg :: String -> State Context ()
+newArg arg = modify $ \st -> st { unArgs = S.insert arg (unArgs st) }
+
+newDecl :: String -> State Context [Int]
 newDecl decl = do
   st <- get
-  put $ st { unWorld  = M.insert decl (decl, unIndex st) (unWorld st)
+  put $ st { unWorld  = M.insert decl (unIndex st) (unWorld st)
            , unIndex  = init (unIndex st) ++ [last (unIndex st) + 1]
-           , unLocals = S.insert decl (unLocals st)
            }
-  return (decl, unIndex st)
+  return $ unIndex st
 
-showDecl :: VarIndex -> String
--- showDecl (decl, i) = decl ++ "⟨" ++ intercalate " " (map show i) ++ "⟩"
-showDecl (decl, i) = decl
+showDecl :: [Int] -> String
+showDecl decl = "⟨" ++ intercalate " " (map show decl) ++ "⟩"
 
 newBlock :: Context -> Context
 newBlock st =
   st { unSS     = id
      , unIndex  = unIndex st ++ [1]
      , unArgs   = S.empty
-     , unLocals = S.empty
      }
-
-addAssign :: String -> State Context ()
-addAssign v =  do
-  st <- get
-  unless (S.member v (unLocals st)) $ do
-    v' <- resolveVar v
-    put $ st { unAssign = S.insert v' (unAssign st) }
-
-{-
-addF :: String -> E -> State Context ()
-addF v e = modify $ \st -> st { unF = M.insert v e (unF st) }
--}
 
 exitBlock :: State Context ()
 exitBlock = do
@@ -70,7 +55,7 @@ exitBlock = do
   put $ st { unIndex  = init (unIndex st) ++ [last (unIndex st) + 1] }
 
 idContext :: Context
-idContext = Context id M.empty [1] S.empty S.empty S.empty M.empty
+idContext = Context id M.empty [1] S.empty
 
 -- function name, impure arguments (0 is this, -1 is world)
 fns :: M.Map String [Int]
@@ -87,14 +72,16 @@ getobj True (VarRef _ (Id _ lv)) = Just lv
 getobj False _                   = Nothing
 getobj _     _                   = Nothing
 
-resolveVar :: String -> State Context Name
+resolveVar :: String -> State Context E
 resolveVar n = do
   st <- get
-  case M.lookup n (unWorld st) of
-    Just n' -> return $ if (S.member n (unLocals st))
-      then Local $ showDecl n'
-      else Bound $ showDecl n'
-    Nothing -> return $ Global n
+  if S.member n (unArgs st)
+    then return $ Ref n
+    else case M.lookup n (unWorld st) of
+      Just n' -> return $ Call (Ref worldFn) [Const (showDecl n'), Ref world]
+      Nothing -> return $ Call (Ref worldFn) [Const n, Ref world]
+      -- Nothing -> return $ Call (Ref worldFn) [Const n, Ref "world"]
+      -- Nothing -> return $ Ref $ "REF<" ++ n ++ ">"
 
 convertE :: Expression a -> State Context E
 convertE (StringLit a lit) = return $ Const lit
@@ -110,27 +97,31 @@ convertE (ArrayLit a es) = App (Lam "array" ((setA (Var "array") 0 (convertE $ h
 convertE (ObjectLit a [(Prop a, Expression a)])
 convertE (ThisRef a)
 -}
-convertE (VarRef a (Id a' ref)) = Ref <$> resolveVar ref
+convertE (VarRef a (Id a' ref)) = resolveVar ref
 convertE (DotRef _ e (Id _ ref)) = do
   e'   <- convertE e
-  return $ Call (Ref $ Global "get") [Const ref, e']
+  return $ Call (Ref "get") [Const ref, e']
 convertE (BracketRef a e i) = do
   e' <- convertE e
   i' <- convertE i
-  return $ Call (Ref $ Global "get_elem") [i', e']
+  return $ Call (Ref "get_elem") [i', e']
 convertE (NewExpr a f xs) = do
   f'  <- convertE f
   xs' <- mapM convertE xs
   return $ Call f' xs'
 convertE (PrefixExpr a op e) = do
   e' <- convertE e
-  return $ Call (Ref $ Global $ show op) [e']
+  return $ Call (Ref $ show op) [e']
 convertE (UnaryAssignExpr a op (LVar a' lv)) = do
   let (opname, push) = op'
   st <- get
   lv' <- resolveVar lv
-  push $ Assign lv' (Call (Ref $ Global opname) [Ref lv'])
-  return $ Ref lv'
+  case M.lookup lv (unWorld st) of
+    Just decl -> do
+      push $ Assign world (Call (Ref worldUpFn) [Const (showDecl decl), Call (Ref opname) [lv'], Ref world])
+    Nothing   -> do
+      push $ Assign world (Call (Ref worldUpFn) [Const lv, Call (Ref opname) [lv'], Ref world])
+  return lv'
   where
     op' = getOp op
     getOp PrefixInc  = ("inc", pushBack )
@@ -140,29 +131,37 @@ convertE (UnaryAssignExpr a op (LVar a' lv)) = do
 convertE (InfixExpr a op l r) = do
   l' <- convertE l
   r' <- convertE r
-  return $ Call (Ref $ Global $ show op) [l', r']
+  return $ Call (Ref $ show op) [l', r']
 convertE (CondExpr a cnd t f) = do
   cnd' <- convertE cnd
   t'   <- convertE t
   f'   <- convertE f
-  return (Call (Ref $ Global "cond") [cnd', Lambda [] (P [Return t']), Lambda [] (P [Return f'])])
+  return (Call (Ref "cond") [cnd', Lambda [] (P [Return t']), Lambda [] (P [Return f'])])
 convertE (AssignExpr a op (LVar a' lv) e) = do
-  st  <- get
   e'  <- convertE e
+  st  <- get
   lv' <- resolveVar lv
-  addAssign lv
-  pushBack $ Assign lv' e'
-  return $ Ref lv'
+
+  case M.lookup lv (unWorld st) of
+    Just decl -> do
+      pushBack $ Assign world (Call (Ref worldUpFn) [Const (showDecl decl), e', Ref world])
+    Nothing   -> do
+      pushBack $ Assign world (Call (Ref worldUpFn) [Const lv, e', Ref world])
+
+  return lv'
 convertE (AssignExpr a op (LDot a' lv fld) rv) = do
   lv' <- convertE lv
   rv' <- convertE rv
   st  <- get
   case getobj True lv of
     Just n -> do
-      n' <- resolveVar n
-      addAssign n
-      pushBack $ Assign n' (Call (Ref $ Global "set") [Ref n', Const fld, rv', lv'])
-      return $ (Call (Ref $ Global "get") [Ref n', Const fld, lv'])
+      case M.lookup n (unWorld st) of
+        Just n' -> do
+          pushBack $ Assign world (Call (Ref worldUpFn) [Const (showDecl n'), Call (Ref "set") [Const fld, rv', lv'], Ref world])
+          return $ (Call (Ref worldFn) [lv', Ref world])
+        Nothing -> do
+          pushBack $ Assign world (Call (Ref worldUpFn) [Const n, Call (Ref "set") [Const fld, rv', lv'], Ref world])
+          return $ (Call (Ref worldFn) [lv', Ref world])
       -- pushBack $ Assign n (Call (Ref "set") [lv', Const fld, rv'])
       -- return $ Ref n
     Nothing -> error "Dot expression without dotref?"
@@ -170,61 +169,42 @@ convertE (ListExpr a es) = do
   let go e@(AssignExpr _ _ _ _) = convertE e
       go e                      = convertE (AssignExpr a OpAssign (LVar a "@") e)
   last <$> mapM go es
-{-
-  o.f(x)
-≈ ρ, σ, ω = f(o, x, ω)
-
-  function f(x)
-≈ function f(this, x, ω)
--}
 convertE (CallExpr a f xs) = do
   f'  <- convertE f
   xs' <- mapM convertE xs
   st  <- get
 
-  pushBack $ Assign (Local result) (Call f' (xs' ++ [Ref $ Local world]))
+  pushBack $ Assign world (Call f' (xs' ++ [Ref world]))
 
   whenJust (getobj False f) $ \n -> do
-    n' <- resolveVar n
-    pushBack $ Assign n' (Call (Ref $ Global worldFn) [Const object, Ref $ Local world])
+    case M.lookup n (unWorld st) of
+      Just n' -> do
+        pushBack $ Assign world (Call (Ref worldUpFn) [Const (showDecl n'), Call (Ref worldFn) [Const object, Ref world], Ref world])
+      Nothing ->
+        pushBack $ Assign world (Call (Ref worldUpFn) [Const n, Call (Ref worldFn) [Const object, Ref world], Ref world])
 
-  return (Ref $ Local result)
+  return $ (Call (Ref worldFn) [Const result, Ref world])
 convertE (FuncExpr a n xs ss) = do
   st <- get
-  let st' = execState (mapM newDecl (map (\(Id _ n) -> n) xs)) (newBlock st)
-      (xs', st'') = flip runState st' $ do
-        mapM_ convertS ss
-        mapM (\(Id _ n) -> resolveVar n) xs
-      ss' = unSS st'' $ []
-      isR (Return _) = True
-      isR _          = False
-      lastR | null ss'  = False
-            | otherwise = isR $ last ss'
-      l   = Lambda (xs' ++ [Local world]) (P $ ss' ++ if lastR then [] else [Return (Ref $ Local world)])
+  let st' = execState (mapM newArg (map (\(Id _ n) -> n) xs)) (newBlock st)
+  let ss' = unSS (execState (mapM_ convertS ss) st') []
+      l   = Lambda (map (\(Id _ n) -> n) xs ++ [world]) (P $ ss' ++ [Return (Ref world)])
 
   exitBlock
 
   case n of
     Just (Id _ n) -> do
-      newDecl n
-      n' <- resolveVar n
-      pushBack $ Assign n' l
-      return l
+      pushBack $ Assign n l
+      return $ Ref n
     _ ->
       return l
 
 convertS :: Statement a -> State Context ()
 convertS (BlockStmt a ss) = do
   st <- get
-  let st' = execState (mapM_ convertS ss) (newBlock st)
-      ss' = unSS st' []
-      isR (Return _) = True
-      isR _          = False
-      lastR | null ss'  = False
-            | otherwise = isR $ last ss'
+  let ss' = unSS (execState (mapM_ convertS ss) (newBlock st)) []
   exitBlock
-  -- pushBack $ Assign (Local result) (Call (Lambda [] (P (ss' ++ if lastR then [] else [Return (Call (Ref $ Global mergeFn) (map Ref $ (S.toList $ unAssign st')))]))) [])
-  pushBack $ Assign (Local world) (Call (Lambda [Local world] (P (ss' ++ if lastR then [] else [Return (Ref $ Local world)]))) [Ref $ Local world])
+  pushBack $ Assign world (Call (Lambda [world] (P (ss' ++ [Return (Ref world)]))) [Ref world])
 convertS (EmptyStmt a) = return ()
 convertS (ExprStmt a e@(AssignExpr _ _ _ _)) = convertE e >> return ()
 convertS (ExprStmt a e) = convertE e >> return ()
@@ -242,7 +222,7 @@ convertS (WhileStmt a e s) = do
   e' <- convertE e
   let s' = unSS (execState (convertS s) (newBlock st)) []
   exitBlock
-  pushBack $ Assign (Local result) (Call (Ref $ Global "while") [e', Lambda [Local world] (P $ s' ++ [Return (Ref $ Local world)])])
+  pushBack $ Assign world (Call (Ref "while") [e', Lambda [world] (P $ s' ++ [Return (Ref world)])])
 {-
 convertS (DoWhileStmt a (Statement a) (Expression a))
 convertS (BreakStmt a (Maybe (Id a)))
@@ -255,12 +235,9 @@ convertS (TryStmt a (Statement a) (Maybe (CatchClause a)) (Maybe (Statement a)))
 convertS (ThrowStmt a (Expression a))
 -}
 convertS (ReturnStmt a (Just e)) = do
-  st <- get
   e' <- convertE e
-  pushBack $ Return e'
-  -- pushBack $ Return (Call (Ref $ Global mergeFn) (e' : (map Ref (S.toList $ unAssign st))))
-  -- pushBack $ Assign world (Call (Ref worldUpFn) [Const result, e', Ref world])
-convertS (ReturnStmt a Nothing) = pushBack $ Return (Ref $ Local world)
+  pushBack $ Assign world (Call (Ref worldUpFn) [Const result, e', Ref world])
+convertS (ReturnStmt a Nothing) = pushBack $ Return (Ref world)
 {-
 convertS (WithStmt a (Expression a) (Statement a))
 -}
@@ -269,8 +246,7 @@ convertS (VarDeclStmt a decls) = do
     decl <- newDecl n
     whenJust e $ \e' -> do
       e'' <- convertE e'
-      n' <- resolveVar n
-      pushBack $ Assign n' e''
+      pushBack $ Assign world (Call (Ref worldUpFn) [Const (showDecl decl), e'', Ref world])
 {-
 convertS (FunctionStmt a (Id a) [Id a] [Statement a])
 -}
@@ -284,7 +260,7 @@ parseExpr str = case parse expression "" str of
   Left err   -> error $ show err
 
 testConvert :: String -> P
-testConvert e = P $ unSS ss [Return $ Ref $ Local world]
+testConvert e = P $ unSS ss [Return (Ref world)]
   where (_, ss)       = runState (convertS $ BlockStmt a pr) idContext
         (Script a pr) = parseProgram e
 
